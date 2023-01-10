203d713807ee0a7fbfe97d142c2564e82adb5624
LooXzy <93411041+LooXzy@users.noreply.github.com>
2023-01-10 17:49:45 +0100

    In development

--- pyipcalc/pyipcalc.py
+++ pyipcalc/pyipcalc.py
@@ -28,6 +28,52 @@ def calc_netmask_with_nbr_hote(nbr_host):
             netmask_cidr = "25"
         elif nbr_host <= 254:
             netmask_cidr = "24"
+        elif nbr_host <= 510:
+            netmask_cidr = "23"
+        elif nbr_host <= 1022:
+            netmask_cidr = "22"
+        elif nbr_host <= 2046:
+            netmask_cidr = "21"
+        elif nbr_host <= 4094:
+            netmask_cidr = "20"
+        elif nbr_host <= 8190:
+            netmask_cidr = "19"
+        elif nbr_host <= 16382:
+            netmask_cidr = "18"
+        elif nbr_host <= 32766:
+            netmask_cidr = "17"
+        elif nbr_host <= 65534:
+            netmask_cidr = "16"
+        elif nbr_host <= 131070:
+            netmask_cidr = "15"
+        elif nbr_host <= 262142:
+            netmask_cidr = "14"
+        elif nbr_host <= 524286:
+            netmask_cidr = "13"
+        elif nbr_host <= 1048574:
+            netmask_cidr = "12"
+        elif nbr_host <= 2097150:
+            netmask_cidr = "11"
+        elif nbr_host <= 4194302:
+            netmask_cidr = "10"
+        elif nbr_host <= 8388606:
+            netmask_cidr = "9"
+        elif nbr_host <= 16777214:
+            netmask_cidr = "8"
+        elif nbr_host <= 33554430:
+            netmask_cidr = "7"
+        elif nbr_host <= 67108862:
+            netmask_cidr = "6"
+        elif nbr_host <= 134217726:
+            netmask_cidr = "5"
+        elif nbr_host <= 268435454:
+            netmask_cidr = "4"
+        elif nbr_host <= 536870910:
+            netmask_cidr = "3"
+        elif nbr_host <= 1073741822:
+            netmask_cidr = "2"
+        elif nbr_host <= 2147483646:
+            netmask_cidr = "1"
     else:
         netmask_cidr = "Le nombre d'hôtes demandés est incorrect"
 
@@ -36,4 +82,4 @@ def calc_netmask_with_nbr_hote(nbr_host):
 if __name__ == '__main__':
     print(calc_nbr_host(24))
     print(convert_decimal_to_binary(192))
-    print(calc_netmask_with_nbr_hote(200))
+    print(calc_netmask_with_nbr_hote(300))
